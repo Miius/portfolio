f74e433c1b93b3b02a6461b3240806f3b3a22611
Matheus Felipe <felipe_2012matheus@hotmail.com>
2025-09-17 13:39:04 -0300

    Adicionando novos projetos e reestruturando a pagina de projetos

--- my-project/src/pages/projectsPage/ProjectsPage.js
+++ my-project/src/pages/projectsPage/ProjectsPage.js
@@ -7,6 +7,8 @@ import Footer from "../../components/footer/Footer";
 
 import imageIntroducion from '../../assets/img/project/introducion.png';
 
+import iconLink from '../../assets/icon/link.svg';
+
 // Imagem dos projetos
 import imageHooked from '../../assets/img/project/hooked.png';
 import imageBrocolli from '../../assets/img/project/broccoli.png';
@@ -14,14 +16,29 @@ import imageBlockar from '../../assets/img/project/blockar.jpg';
 import imageChaotic from '../../assets/img/project/chaotic.png';
 import imageTooMuch from '../../assets/img/project/toomuch.png';
 import imageTransitandoLegal from '../../assets/img/project/legaldigital.png';
+import imageItauRunner from '../../assets/img/project/itaurunner.webp';
+import imageMeuMalvadoFav from '../../assets/img/project/malvadoFav.webp';
+import imageTimMapa from '../../assets/img/project/timMapa.png';
+import imageDiveb from '../../assets/img/project/diveb.png';
+import imageTimBlackFriday from '../../assets/img/project/timBlackFriday.png';
+import imagefonteOuro from '../../assets/img/project/fonteOuro.png';
+import imagecafeAioros from '../../assets/img/project/cafeAioros.png';
+
 
 const allProjectsData = [
     { id: 1, path: 'https://play.google.com/store/apps/details?id=com.Aioros.TLDigital&hl=pt_BR', title: 'Transitando Legal Digital', category: 'Aioros Studios', image: imageTransitandoLegal, tags: ['Unity', 'Android/iOS', 'Educational'], type: 'professional' },
-    { id: 2, path: 'https://mamacabra.itch.io/hooked', title: 'Hooked', category: 'Indie', image: imageHooked, tags: ['Unity', 'Windows', 'Linux'], type: 'professional' },
-    { id: 3, path: 'https://mamacabra.itch.io/blockar', title: 'Blockar ', category: 'Indie', image: imageBlockar, tags: ['Unity', 'Windows', 'Linux'], type: 'professional' },
-    { id: 4, path: 'https://mamacabra.itch.io/chaoticsea', title: 'Chaotic Sea', category: 'Indie', image: imageChaotic, tags: ['Unity', 'Windows', 'Linux'], type: 'professional' },
+    { id: 2, path: 'https://mamacabra.itch.io/hooked', title: 'Hooked', category: 'Indie', image: imageHooked, tags: ['Unity', 'Windows', 'Linux'], type: 'personal' },
+    { id: 3, path: 'https://mamacabra.itch.io/blockar', title: 'Blockar ', category: 'Indie', image: imageBlockar, tags: ['Unity', 'Windows', 'Linux'], type: 'personal' },
+    { id: 4, path: 'https://mamacabra.itch.io/chaoticsea', title: 'Chaotic Sea', category: 'Indie', image: imageChaotic, tags: ['Unity', 'Windows', 'Linux'], type: 'personal' },
     { id: 5, path: 'https://giovana-melo.itch.io/too-much-work-for-only-one-godess', title: 'Too Much Work For Only One Godess', category: 'Game Jam', image: imageTooMuch, tags: ['Construct', 'Windows', 'Linux'], type: 'personal' },
     { id: 6, path: 'https://giovana-melo.itch.io/brocolli', title: 'Brocolli ', category: 'Game Jam', image: imageBrocolli, tags: ['Construct', 'Windows', 'Linux'], type: 'personal' },
+    { id: 7, path: 'https://drive.google.com/file/d/1TeISWDMRWdXyG_1ntQQTxJAHQrSaIXkX/view?usp=sharing', title: 'Itaú Runner ', category: 'Aioros Studios', image: imageItauRunner, tags: ['Unity', 'Phygital'], type: 'professional' },
+    { id: 8, path: 'https://drive.google.com/file/d/19iqXeNrILq1pDC6wQ42d2_sBb4UX82qZ/view?usp=sharing', title: 'Campanha Meu Malvado Favorito 4', category: 'Aioros Studios', image: imageMeuMalvadoFav, tags: ['Unity', 'Phygital'], type: 'professional' },
+    { id: 9, path: 'https://drive.google.com/file/d/1ERE4YnOgKc3QG0Asa8Li6amVI-kMwEOI/view?usp=sharing', title: 'Tim Mapa Interativo', category: 'Aioros Studios', image: imageTimMapa, tags: ['Unity', 'Phygital'], type: 'professional' },
+    { id: 10, path: 'https://drive.google.com/file/d/1hZLAMKMcvmLr7XEQFE2NqvefhXRpH4c9/view?usp=sharing', title: 'Dive.b Learning English', category: 'Aioros Studios', image: imageDiveb, tags: ['Unity', 'PC', 'Educational'], type: 'professional' },
+    { id: 11, path: 'https://drive.google.com/file/d/1gY_kbKFIFmwMIyEKz9Qzw-Rt1v4YHSf4/view?usp=sharing', title: 'Tim Black Friday', category: 'Aioros Studios', image: imageTimBlackFriday, tags: ['Unity', 'Phygital'], type: 'professional' },
+    { id: 12, path: 'https://drive.google.com/file/d/1idGE-0z4tB4uDBrNIU1OrG7gb7PIBz2L/view?usp=sharing', title: 'Banco do Brasil - Fonte de Ouro', category: 'Aioros Studios', image: imagefonteOuro, tags: ['Unity', 'Phygital'], type: 'professional' },
+    { id: 13, path: 'https://drive.google.com/file/d/1lXTRqFogZmb2u8Hi9R2wTRbFdTwhHReL/view?usp=sharing', title: 'Aioros Café', category: 'Aioros Studios', image: imagecafeAioros, tags: ['Unity', 'Phygital'], type: 'professional' },
 ];
 
 const ProjectsPage = () => {
@@ -47,6 +64,10 @@ const ProjectsPage = () => {
                 <div className="header-projectsAll">
                     <h1 className="header-title-projectsAll">{texts.TitlePage_ProjectsProfessional}</h1>
                     <div className="header-divider-projectsAll"/>
+                </div>
+                <div className="header-link-projectsAll">
+                    <a  className="text-link-projectsAll" href="https://www.aioros.com.br/#Cases" target="_blank" rel="noopener noreferrer">Projetos Aioros Studios</a>
+                    <img className="icon-link-projectsAll" src={iconLink} alt="icone link" />
                 </div>
                 <div className="projectsAll-grid">
                     {professionalProjects.map(project => (
